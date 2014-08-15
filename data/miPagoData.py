484e78d605390ff08044ec2ba15a77e30d677807
tanito <ismae.vayra@gmail.com>
2014-08-14 21:53:28 -0300

    agregada la parte de datos del web server

---
+++ data/miPagoData.py
@@ -0,0 +1,22 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+
+from data.core.endpoints import data_handlers
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = data_handlers
+        tornado.web.Application.__init__(self, handlers)
+
+
+def main():
+    app = Application()
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(9000)
+
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
