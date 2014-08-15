72403c38ce8f5fcf15963e5e0fdf5153431e284d
tanito <ismae.vayra@gmail.com>
2014-08-14 21:51:18 -0300

    agregado el endpoint para los archivos estaticos

--- web/core/endpoints.py
+++ web/core/endpoints.py
@@ -1,6 +1,10 @@
 from web.core.handlers.main_handler import MainHandler
+import tornado.web
 
 
 web_handlers = [
     (r"/", MainHandler),
+
+#     Static handlers
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static_path"}),
 ]
