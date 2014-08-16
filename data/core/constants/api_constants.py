be8c23a31813cb72d69c9f87d1cb2b33546d37ec
tanito <ismae.vayra@gmail.com>
2014-08-16 04:02:01 -0300

    se agrego la conexion a la base de datos local de prueba y se creo una tabla de usuarios atravez de modelo

---
+++ data/core/constants/api_constants.py
@@ -0,0 +1,6 @@
+DATABASE = {
+    "database": "mipago",
+    "adminUser": "mipagoadmin",
+    "password": "yatoypalabemba",
+    "host_db": "localhost",
+}
---
+++ data/core/models/db/db_connection.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+
+from data.core.constants.api_constants import DATABASE
+
+
+class Singleton(type):
+
+    def __init__(cls, name, bases, dct):
+        cls.__instance = None
+        type.__init__(cls, name, bases, dct)
+
+    def __call__(cls):
+        if cls.__instance is None:
+            cls.__instance = type.__call__(cls)
+        return cls.__instance
+
+
+class DbConnection:
+    """
+    This class do the db connection using singleton
+    """
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        self.engine = None
+        self.set_db_connection()
+
+    def get_db_connection(self):
+        return self.engine
+
+    def set_db_connection(self):
+        self.engine = create_engine("postgresql://scott:tiger@localhost:5432/mydatabase")
+        self.engine = create_engine("postgresql://%s:%s@%s/%s"
+                                    %(DATABASE['adminUser'],
+                                      DATABASE['password'],
+                                      DATABASE['host_db'],
+                                      DATABASE['database'],
+        ))
