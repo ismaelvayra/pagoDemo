964c78c31454c967f85db5578425b5b735d5b087
ismaelvayra <ismael.vayra>
2014-08-30 12:02:35 -0300

    empty index test html

--- web/core/endpoints.py
+++ web/core/endpoints.py
@@ -1,9 +1,10 @@
-from web.core.handlers.main_handler import MainHandler
+from web.core.handlers.main_handler import MainHandler, TestHandler
 import tornado.web
 
 
 web_handlers = [
     (r"/", MainHandler),
+    (r"/test", TestHandler),
 
 #     Static handlers
     (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static_path"}),
--- web/core/handlers/main_handler.py
+++ web/core/handlers/main_handler.py
@@ -4,3 +4,8 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
+
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("test.html")
